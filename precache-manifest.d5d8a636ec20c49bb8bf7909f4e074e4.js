c877e615e13c1c757f46f54030029424e02f6237
asif2305 <mygithub2305@gmail.com>
2020-05-08 22:28:34 +0200

    Updates

---
+++ precache-manifest.d5d8a636ec20c49bb8bf7909f4e074e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae585f64ad5f238ffbe2795aa9743550",
+    "url": "/TrackingCovid19/index.html"
+  },
+  {
+    "revision": "d9dd043091ca00ac3795",
+    "url": "/TrackingCovid19/static/css/main.6fc7a576.chunk.css"
+  },
+  {
+    "revision": "ba7b0fab0b90fd0eabfe",
+    "url": "/TrackingCovid19/static/js/2.b5bd2def.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/TrackingCovid19/static/js/2.b5bd2def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9dd043091ca00ac3795",
+    "url": "/TrackingCovid19/static/js/main.05e3a2e2.chunk.js"
+  },
+  {
+    "revision": "74d2d4d92d4ec9e31e54",
+    "url": "/TrackingCovid19/static/js/runtime-main.27e18f08.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/TrackingCovid19/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TrackingCovid19/static/media/logo.5d5d9eef.svg"
+  }
+]);
